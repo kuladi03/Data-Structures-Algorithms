eb4f0d279a63a962cd4e5b0e206487eaec047f18
kuladi03 <kuladi03@gmail.com>
2023-10-19 18:44:02 +0530

    Added trees

--- Code/BinaryTree/intro.cpp
+++ Code/BinaryTree/intro.cpp
@@ -1,8 +1,40 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        this -> data = value;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+Node* buildtree(Node* root){
+        int data;
+        cout<<"Enter the data : "<<endl;
+        cin>>data;
+
+        if(data == -1){
+            return root;
+        }
+        cout<<"Enter the data of left "<<data<<endl;
+        root->left = buildtree(root->left);
+        cout<<"Enter the data of right "<<data<<endl;
+        root->right = buildtree(root->right);
+
+        return root;
+    }
 
 int main(){
 
-    cout<<"Hello World"<<endl;
+    Node* root = NULL;
+    buildtree(root);
+    
     return 0;
 }
